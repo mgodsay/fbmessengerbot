aca0c66fd5c25e2ded1c3f8280a23d2e95978fb8
Mugdha Kulkarni <mukulkarni@ebay.com>
2016-04-27 13:14:19 -0700

    fixed runtime error

--- appengine/hello-world/app.js
+++ appengine/hello-world/app.js
@@ -42,7 +42,7 @@ app.get('/webhook/', function (req, res) {
 
 
 app.post('/webhook/', jsonParser, function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
+  var messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
